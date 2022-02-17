9c22c127938c73c532ac06e4f0dfe75120f1afab
carmine92 <carmine.992@gmail.com>
2022-02-17 23:41:41 +0100

    init marked in div

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import './App.css';
 import React from 'react';
+import { marked } from "marked";
 
 class App extends React.Component{
 
-  textChanged(event){
+  constructor(props){
+    super(props);
+    this.state = {
+      basetext:"",
+      markedhtml:""
+    };
+  }
 
+  textChanged(event){
+    this.setState(state => ({
+      basetext: event.target.value,
+      markedhtml: marked.parse(state.basetext ? state.basetext: ""),
+    }))
   }
 
   render(){
     return (
       <div className="App">
         <div className="mainContainer">
-          <textarea className="inputMarkup" oninput={this.textChanged.bind(this)}>
+          <textarea className="inputMarkup" onInput={this.textChanged.bind(this)} value={this.state.basetext}>
           </textarea>
           <div className="markupPreview">
+            {this.state.markedhtml}
           </div>
         </div>
       </div>
